37d8e641997d57951daa4c37bf594af5fb0a985a
Henry Chun <chunhyungjoon@gmail.com>
2022-09-18 10:16:53 +1000

    done identification

--- main.py
+++ main.py
@@ -1,31 +1,51 @@
 # ATM applicatoin
 
+# import sys
 # import prettytable module
 from prettytable import PrettyTable
 # import transaction modules
 from modules import authentication
 
-print('ATM applicatoin')
+print('=== < ATM Applicatoin > ====')
 
 menu_id = []
 # design manu
 
 
 def transaction_menu():
-    x = PrettyTable()
-    x.field_names = ['Transaction ID', 'Transaction Type']
-    x.add_row(['1', 'Withdrawal'])
-    x.add_row(['2', 'Deposit'])
-    x.add_row(['3', 'Transfer'])
-    x.add_row(['4', 'Balance'])
-    x.add_row(['5', 'Cancel Transaction'])
-    print(x.get_string(title='<<< Select a transaction >>>'))
-    for id, menu in x.rows:
+    table_x = PrettyTable()
+    table_x.field_names = ['Transaction ID', 'Transaction Type']
+    table_x.add_row(['1', 'Withdrawal'])
+    table_x.add_row(['2', 'Deposit'])
+    table_x.add_row(['3', 'Transfer'])
+    table_x.add_row(['4', 'Balance'])
+    table_x.add_row(['5', 'Cancel Transaction'])
+    print(table_x.get_string(title='<<< Select a transaction >>>'))
+    for id, menu in table_x.rows:
         menu_id.append(id)
-    print(menu_id)
 
 
-transaction_menu()
-# if __name__ == '__main__':
-#     next = authentication.verity_identification()
-#     if not next:
+# transaction_menu()
+if __name__ == '__main__':
+    # verify identification
+    next_step = authentication.verity_identification()
+    # if PIN is correct?
+    if next_step:
+        while True:
+            transaction_menu()
+            # ask a client to enter the menu id
+            input_id = input('Please select a transaction number : ')
+            # chack if the item is in the menu list
+            try:
+                if input_id in menu_id:
+                    print(f' you selected id : {input_id} ')
+                    break
+                else:
+                    print('ID not found!!')
+            # exception block
+            except Exception:
+                print('No ID, please select it again!!')
+    #  wrong PIN with 3 times
+    else:
+        print(' You have entered wrong PIN number, Please check your PIN.')
+        exit()
