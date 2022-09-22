33972db0f3fe1b2248f739628cf5ce14a0e918f2
Henry Chun <chunhyungjoon@gmail.com>
2022-09-23 09:53:06 +1000

    fit pep8 in test modules

--- modules/test_transaction_trasfer.py
+++ modules/test_transaction_trasfer.py
@@ -1,58 +1,68 @@
 from transactions import Transactions, Transfer
 import pytest
 
-# inputs = iter([200, 0, -500, 'asdf']) for verifying values of input_bankdetails
+# inputs = iter([200, 0, -500, 'asdf']) for verifying values of
+# input_bankdetails
 inputs = iter([12001, 455112, 0, 'asdf'])
 # verifying input amount for transfer
 amount = iter([200, 300, 0, 'asdf'])
 
+
 @pytest.fixture
 def trans():
-	return Transactions('3', 10000)
+    return Transactions('3', 10000)
+
 
 @pytest.fixture
 def transfer():
     return Transfer('3', 10000)
 
-#test input bsb 12001, account no 45112
+# test input bsb 12001, account no 45112
+
+
 def test_input_transfer_bankdetails(transfer, monkeypatch):
-	monkeypatch.setattr('builtins.input', lambda prompt: next(inputs))
-	assert transfer.input_bankdetails() == "12001' | '455112"
-	# assert transfer.input_bankdetails() == 455112
+    monkeypatch.setattr('builtins.input', lambda prompt: next(inputs))
+    assert transfer.input_bankdetails() == "12001' | '455112"
+    # assert transfer.input_bankdetails() == 455112
 
-#test input bsb 0, bsb 'asdf'
+# test input bsb 0, bsb 'asdf'
+
+
 def test_input_transfer_bankdetails_odds(transfer, monkeypatch):
-	monkeypatch.setattr('builtins.input', lambda prompt: next(inputs))
-	# with pytest.raises(StopIteration):
-	with pytest.raises(StopIteration):
-		transfer.input_bankdetails()
+    monkeypatch.setattr('builtins.input', lambda prompt: next(inputs))
+    # with pytest.raises(StopIteration):
+    with pytest.raises(StopIteration):
+        transfer.input_bankdetails()
 
 
-#test 200, 300
+# test 200, 300
 def test_input_transfer_amount(trans, monkeypatch):
-	monkeypatch.setattr('builtins.input', lambda prompt: next(amount))
-	assert trans.input_amount('3') == 200
-	assert trans.input_amount('3') == 300
+    monkeypatch.setattr('builtins.input', lambda prompt: next(amount))
+    assert trans.input_amount('3') == 200
+    assert trans.input_amount('3') == 300
 
-#test 0, 'asdf'
+# test 0, 'asdf'
+
+
 def test_input_transfer_amount_odds(trans, monkeypatch):
-	monkeypatch.setattr('builtins.input', lambda prompt: next(amount))
-	with pytest.raises(StopIteration):
-		trans.input_amount('3')
+    monkeypatch.setattr('builtins.input', lambda prompt: next(amount))
+    with pytest.raises(StopIteration):
+        trans.input_amount('3')
 
 
-#test check balance
+# test check balance
 def test_check_balance(trans):
-	tot = trans.check_balance(200, 0) # when balance= $10000 transferred $200
-	print('******* balance : ', tot)    # check if balance is correct
-	trans.check_balance(12000, 0)  # when balance = $10000 withdrawn $1200 -> Insufficient funds
+    tot = trans.check_balance(200, 0)  # when balance= $10000 transferred $200
+    print('******* balance : ', tot)    # check if balance is correct
+    # when balance = $10000 withdrawn $1200 -> Insufficient funds
+    trans.check_balance(12000, 0)
 
-#display balance
+# display balance
+
+
 def test_display_balance(trans):
     trans.display_balance(9800)
 
-
-	# test_input = Mock(side_effect=input_list)
-	# test_input = Mock(return_value=input_list)
-	# assert test_input == 200
-
+    # test_input = Mock(side_effect=input_list)
+    # test_input = Mock(return_value=input_list)
+    # assert test_input == 200
--- modules/test_transaction_withdraw.py
+++ modules/test_transaction_withdraw.py
@@ -4,34 +4,42 @@ import pytest
 # inputs = iter([200, 0, -500, 'asdf']) for verifying values of input_amount
 inputs = iter([200, 300, 0, 'asdf'])
 
+
 @pytest.fixture
 def trans():
-	return Transactions('1', 10000)
+    return Transactions('1', 10000)
 
-#test 200, 300
+# test 200, 300
+
+
 def test_input_withdrawal_amount(trans, monkeypatch):
-	monkeypatch.setattr('builtins.input', lambda prompt: next(inputs))
-	assert trans.input_amount('1') == 200
-	assert trans.input_amount('1') == 300
+    monkeypatch.setattr('builtins.input', lambda prompt: next(inputs))
+    assert trans.input_amount('1') == 200
+    assert trans.input_amount('1') == 300
 
-#test 0, 'asdf'
+# test 0, 'asdf'
+
+
 def test_input_withdrawal_amount_odds(trans, monkeypatch):
-	monkeypatch.setattr('builtins.input', lambda prompt: next(inputs))
-	with pytest.raises(StopIteration):
-		trans.input_amount('1')
+    monkeypatch.setattr('builtins.input', lambda prompt: next(inputs))
+    with pytest.raises(StopIteration):
+        trans.input_amount('1')
 
-#test check balance
+# test check balance
+
+
 def test_check_balance(trans):
-	tot = trans.check_balance(200, 0) # when balance= $10000 withdrawn $200
-	print('******* balance : ', tot)    # check if balance is correct
-	trans.check_balance(12000, 0)  # when balcn= $10000 withdrawn $1200 -> Insufficient funds
+    tot = trans.check_balance(200, 0)  # when balance= $10000 withdrawn $200
+    print('******* balance : ', tot)    # check if balance is correct
+    # when balance = $10000 withdrawn $1200 -> Insufficient funds
+    trans.check_balance(12000, 0)
 
-#display balance
+# display balance
+
+
 def test_display_balance(trans):
     trans.display_balance(9800)
 
-
-	# test_input = Mock(side_effect=input_list)
-	# test_input = Mock(return_value=input_list)
-	# assert test_input == 200
-
+    # test_input = Mock(side_effect=input_list)
+    # test_input = Mock(return_value=input_list)
+    # assert test_input == 200
