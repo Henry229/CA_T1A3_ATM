251ccf38f0a5c872e919da03e098e7ff4967ccd8
Henry Chun <chunhyungjoon@gmail.com>
2022-09-25 18:45:11 +1000

    fix bashfile

--- t1a3_atm.sh
+++ t1a3_atm.sh
@@ -1,35 +1,19 @@
 #!/bin/bash
-# if [ -x "$(command -v reuqirements.txt)" ]; then
-#   echo 'Will install requirements.txt.' >&2
-#   pip install -r requirements.txt
-# fi
 
-# if [[ -x "$(command -v python)" ]]
-# then
-#     pyv="$(python -V 2>&1)"
-#     if [[ $pyv == "Python 3"* ]]
-#     then
-#         python3 main.py $1
-#     else
-#         echo "You don't have Python 3.x version of python, please, sort it out!" >&2
-#     fi 
-# else
-#     echo "You don't have python! please, install if first!" >&2
-# fi
-
-printf 'Authorisation required for activating virtual environment and download: \n'
+printf 'Will show you a package list : \n'
 cat requirements.txt
-echo "Do you authorise to proceed? [Y,n]"
+echo "Do you want to proceed? (y,n)"
 read input
 if [[ $input == "Y" || $input == "y" ]]; then
-    echo "Creaing & Activaing Virtual Environment..."
+    echo ">>>>> Creaing  Virtual Environment..."
     python3 -m venv .venv 
+    echo "***** Activaing Virtual Environment..."
     source .venv/bin/activate
     echo "Installing packages..."
     pip install -r requirements.txt
     python3 main.py $1
     exit 0
 else
-    echo "Program terminating"
+    echo "Script will be terminated"
     exit 1
 fi
